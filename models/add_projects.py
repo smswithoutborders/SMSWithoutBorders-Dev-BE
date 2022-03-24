63586871f3b46f0f2576c40c3410822606ed1d42
Promise Fru <promisefru1@gmail.com>
2022-03-24 15:53:09 +0100

    updated add_product model

--- models/add_projects.py
+++ models/add_projects.py
@@ -1,9 +1,13 @@
 import logging
 
-from error import Conflict, Forbidden, InternalServerError
+from error import Conflict, Forbidden, InternalServerError, Unauthorized
 
 import peewee as pw
-from schemas import Users_projects, Projects
+import requests
+from schemas import Users_projects, Projects, Users
+from products import openapi
+
+from configparser import ConfigParser
 
 LOG = logging.getLogger(__name__)
 
@@ -15,17 +19,60 @@ def add_projects(uid, project_name):
         try:
             pid = Projects.get(Projects.name == project_name)
         except Projects.DoesNotExist:
-            LOG.error("INVALID PROJECT")
+            LOG.error("INVALID PRODUCT")
             raise Forbidden()
 
-        add_project = Users_projects.get_or_create(user_id=uid, project_id=pid)
+        try:
+            user = Users.get(Users.id == uid)
+        except Users.DoesNotExist:
+            LOG.error("USER DOESN'T EXIST")
+            raise Forbidden()
 
-        if add_project[1] == True:
+        try:
+            Users_projects.get(
+                Users_projects.user_id == uid, Users_projects.project_id == pid
+            )
+        except Users_projects.DoesNotExist:
+            LOG.debug(f"requesting for {project_name}'s subscription for {uid} ...")
+
+            AUTH_ID = user.auth_id
+            AUTH_KEY = user.auth_key
+
+            setup = ConfigParser()
+            setup.read("setup.ini")
+            SETUP = setup["CREDENTIALS"]
+            SETUP_ID = SETUP["ID"]
+            SETUP_KEY = SETUP["key"]
+
+            data = {
+                "auth_id": AUTH_ID,
+                "auth_key": AUTH_KEY,
+                "id": SETUP_ID,
+                "key": SETUP_KEY,
+            }
+
+            if project_name == "openapi":
+                HOST = openapi.HOST
+                PORT = openapi.PORT
+                VERSION = openapi.VERSION
+                URL = f"{HOST}:{PORT}/{VERSION}/subscribe"
+
+                response = requests.post(url=URL, json=data)
+                if response.status_code == 401:
+                    LOG.error("INVALID SETUP CREDENTIALS")
+                    raise Unauthorized()
+                elif response.status_code == 200:
+                    pass
+            else:
+                LOG.error("INVALID PRODUCT")
+                raise Forbidden()
+
+            Users_projects.create(user_id=uid, project_id=pid)
             LOG.info(f"SUCCESSFULLY SUBSCRIBED {uid} FOR {project_name}")
             return True
-        else:
-            LOG.error(f"{uid} ALREADY SUBSCRIBED FOR {project_name}")
-            raise Conflict()
+
+        LOG.error(f"{uid} ALREADY SUBSCRIBED FOR {project_name}")
+        raise Conflict()
 
     except (pw.DatabaseError) as err:
         raise InternalServerError(err)
---
+++ products.py
@@ -0,0 +1,10 @@
+class Product(object):
+    HOST = ""
+    PORT = ""
+    VERSION = ""
+
+
+class openapi(Product):
+    HOST = "http://127.0.0.1"
+    PORT = "13000"
+    VERSION = "v1"
