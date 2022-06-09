a7d3f8286e0c608763fe8d797ed056a4bafd41bb
Promise Fru <promisefru1@gmail.com>
2022-06-09 15:39:09 +0100

    update: docs

--- models/isSSL.py
+++ models/isSSL.py
@@ -14,13 +14,13 @@ def isSSL(path_crt_file: str, path_key_file: str, path_pem_file: str) -> dict:
         return False
     else:
         if not os.path.exists(path_crt_file):
-            error = "crt_file file not found at %s" % path_crt_file
+            error = "crt_file file not found at '%s'" % path_crt_file
             raise FileNotFoundError(error)
         elif not os.path.exists(path_key_file):
-            error = "key_file file not found at %s" % path_key_file
+            error = "key_file file not found at '%s'" % path_key_file
             raise FileNotFoundError(error)
         elif not os.path.exists(path_pem_file):
-            error = "pem_file file not found at %s" % path_pem_file
+            error = "pem_file file not found at '%s'" % path_pem_file
             raise FileNotFoundError(error)
         else:
             privateKey = open(SSL["KEY"], "r").read()
